da5a3ebddb2cc032dd312f5da3172a4a37aaf33b
Biz <github@bisrat.dev>
2023-06-27 12:02:51 -0600

    add db connection alerts

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,13 @@
 const { connect, connection } = require("mongoose");
+require("dotenv").config();
+const { Clog } = require("../utils/clog");
 
 const connectionString =
-	process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/studentsDB";
+	process.env.MONGODB_URI ||
+	`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
 connect(connectionString);
+const clog = new Clog(connectionString, false);
+clog.info("connecting...");
 
 module.exports = connection;
