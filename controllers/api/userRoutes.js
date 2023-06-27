6a496893d7e3047a7b5eac74aa959f7d8c86c8f9
Biz <github@bisrat.dev>
2023-06-27 12:03:23 -0600

    add get all for users

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,3 +1,16 @@
+const { User } = require("../../models/user");
+const { ClogHttp } = require("../../utils/clog");
 const router = require("express").Router();
+
+router.get("/", async (req, res) => {
+	const clog = new ClogHttp("/api/users", true);
+	try {
+		clog.httpStatus(501, "route not implmented");
+		res.sendStatus(501);
+	} catch (err) {
+		clog.httpStatus(500, err.message);
+		res.sendStatus(500);
+	}
+});
 
 module.exports = router;
