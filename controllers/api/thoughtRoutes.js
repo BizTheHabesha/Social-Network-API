b654a7fdad0bbf62f011e68d7167cdd0d7f52383
Biz <github@bisrat.dev>
2023-06-29 16:20:58 -0600

    add get routes

--- controllers/api/thoughtRoutes.js
+++ controllers/api/thoughtRoutes.js
@@ -1,3 +1,47 @@
+const { isValidObjectId } = require("mongoose");
+const { Thought } = require("../../models");
+const { ClogHttp } = require("../../utils/clog");
 const router = require("express").Router();
+
+router.get("/", async (req, res) => {
+	const clog = new ClogHttp("GET /api/thoughts", true);
+	try {
+		const findRes = await Thought.find();
+		if (!findRes) {
+			clog.httpStatus(204, "Find response returned empty!");
+			res.status(204);
+			return;
+		}
+		clog.httpStatus(200);
+		res.status(200).json(findRes);
+	} catch (err) {
+		clog.error(err.stack);
+		clog.httpStatus(500, err.message);
+		res.sendStatus(500);
+	}
+});
+
+router.get("/:id", async (req, res) => {
+	const clog = new ClogHttp("GET /api/thoughts/:id", true);
+	try {
+		const _id = req.params["id"];
+		if (!isValidObjectId(_id)) {
+			clog.httpStatus(406, `${_id} is not a valid id`);
+			res.status(406).json({ message: `${_id} is not a valid id` });
+			return;
+		}
+		const findRes = await Thought.findById(_id);
+		if (!findRes) {
+			clog.httpStatus(404);
+			res.sendStatus(404);
+		}
+		clog.httpStatus(200);
+		res.status(200).json(findRes);
+	} catch (err) {
+		clog.error(err.stack);
+		clog.httpStatus(500, err.message);
+		res.sendStatus(500);
+	}
+});
 
 module.exports = router;
