322c5ad98af531d6ecdcba878c25ff2e9e54e798
Biz <github@bisrat.dev>
2023-06-29 21:05:30 -0600

    add put route

--- controllers/api/thoughtRoutes.js
+++ controllers/api/thoughtRoutes.js
@@ -112,4 +112,43 @@ router.post("/", async (req, res) => {
 	}
 });
 
+router.put("/:id", async (req, res) => {
+	const clog = new ClogHttp("PUT /api/thoughts/:id", true);
+	try {
+		const _id = req.params["id"];
+		const { thoughtText } = req.body;
+		if (!isValidObjectId(_id)) {
+			clog.httpStatus(406, `${_id} is not a valid id`);
+			res.status(406).json({ message: `${_id} is not a valid id` });
+			return;
+		}
+		const findThoughtRes = await Thought.findById(_id);
+		if (!findThoughtRes) {
+			clog.httpStatus(
+				404,
+				`'${_id}' does not exist or could otherwise not be found`
+			);
+			res.status(404).json({
+				message: `'${_id}' does not exist or could otherwise not be found`,
+			});
+			return;
+		}
+		if (!thoughtText) {
+			clog.httpStatus(406, "Expected thoughtText in request body");
+			res.status(406).json({
+				message: "Expected thoughtText in request body",
+			});
+			return;
+		}
+		await Thought.updateOne({ _id }, { thoughtText }, { new: true });
+		const findUpdatedRes = await Thought.findById(_id);
+		clog.httpStatus(202);
+		res.status(202).json(findUpdatedRes);
+	} catch (err) {
+		clog.error(err.stack);
+		clog.httpStatus(500, err.message);
+		res.sendStatus(500);
+	}
+});
+
 module.exports = router;
